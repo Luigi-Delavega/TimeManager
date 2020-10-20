04e1d5ca2f6a0770047f94d72cda75b7f0008909
crg63 <floriancargouet@gmail.com>
2020-10-20 11:23:28 +0200

    clock

---
+++ theme01/lib/gotham/export/clock.ex
@@ -0,0 +1,19 @@
+defmodule Gotham.Export.Clock do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "clocks" do
+    field :status, :boolean, default: false, null: false
+    field :time, :naive_datetime, null: false
+    field :user, :id, null: false
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(clock, attrs) do
+    clock
+    |> cast(attrs, [:time, :status])
+    |> validate_required([:time, :status])
+  end
+end
