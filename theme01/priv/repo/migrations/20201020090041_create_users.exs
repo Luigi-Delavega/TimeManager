9a92f7aebd26026cde11f7ead0c6beeddfc90ed9
crg63 <floriancargouet@gmail.com>
2020-10-20 13:51:36 +0200

    userID

--- theme01/priv/repo/migrations/20201020090041_create_users.exs
+++ theme01/priv/repo/migrations/20201020090041_create_users.exs
@@ -2,7 +2,8 @@ defmodule Gotham.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    create table(:users, primary_key: false) do
+      add :userID, :id, primary_key: true
       add :username, :string, null: false
       add :email, :string, null: false
 
