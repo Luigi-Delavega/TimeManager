7724426abfc54dcb3ac4657ccc308a2345a16023
crg63 <floriancargouet@gmail.com>
2020-10-21 10:11:56 +0200

    clock

--- theme01/lib/gotham/export/user.ex
+++ theme01/lib/gotham/export/user.ex
@@ -14,11 +14,7 @@ defmodule Gotham.Export.User do
     user
     |> cast(attrs, [:username, :email])
     |> validate_required([:username, :email])
-<<<<<<< HEAD
     |> validate_format(:email, ~r/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/)
     |> unique_constraint(:email)
-=======
-    |> validate_format(:email, ~r/@/)
->>>>>>> 363a1183c6694d10218d641be6676329d9228065
   end
 end
