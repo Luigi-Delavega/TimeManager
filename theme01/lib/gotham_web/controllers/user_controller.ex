70054121f0adc2d3000a30cdc0a6ec5a5cfeec3c
KilliannH <killiannhervagault@gmail.com>
2020-10-22 13:38:47 +0200

    repared get all users

--- theme01/lib/gotham_web/controllers/user_controller.ex
+++ theme01/lib/gotham_web/controllers/user_controller.ex
@@ -6,6 +6,11 @@ defmodule GothamWeb.UserController do
 
   action_fallback GothamWeb.FallbackController
 
+  def getAll(conn, _params) do
+    users = Export.list_users()
+    render(conn, "index.json", users: users)
+  end
+
   def index(conn, %{"username" => _username, "email" => _email} = params) do
     users = Export.get_user_by_mail_name!(params)
     render(conn, "index.json", users: users)
--- theme01/lib/gotham_web/router.ex
+++ theme01/lib/gotham_web/router.ex
@@ -11,12 +11,16 @@ defmodule GothamWeb.Router do
   scope "/api", GothamWeb do
     pipe_through :api
     options "/users", UserController, :create
+
+    get "/users", UserController, :getAll
     resources "/users", UserController, except: [:new, :edit]
 
     resources "/clocks", ClockController, only: [:index]
+    # required for post new clock & getAll by userId
     get("/clocks/:user_id", ClockController, :show)
     post("/clocks/:user_id", ClockController, :create)
     
+    # required for post new workingtime & getAll by userId
     get("/workingtimes/:user_id", WorkingtimesController, :show)
     get("/workingtimes/:user_id/:id", WorkingtimesController, :showone)
     get("/workingtimes/:user_id", WorkingtimesController, :index)
