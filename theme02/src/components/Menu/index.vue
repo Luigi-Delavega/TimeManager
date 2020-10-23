381d7c5a10b443a546265419cb2f5343042ddfb6
crg63 <floriancargouet@gmail.com>
2020-10-24 01:48:00 +0200

    fix menu link

--- theme02/src/components/Menu/index.vue
+++ theme02/src/components/Menu/index.vue
@@ -1,35 +1,43 @@
 <template>
   <div class="hidden">
-    <vs-sidebar absolute hover-expand reduce open>
+    <vs-sidebar fixed hover-expand reduce v-model="active" open>
       <template #logo>
         <img
           src="https://newsroom.ionis-group.com/wp-content/uploads/2018/12/epitech-logo-quadri-baseline-anglais.png"
         />
       </template>
-      <vs-sidebar-item id="dashboard">
-        <template #icon>
-          <font-awesome-icon icon="chart-line" />
-        </template>
-        <router-link :to="{ path: 'dashboard' }">Dashboard</router-link>
-      </vs-sidebar-item>
-      <vs-sidebar-item id="search">
-        <template #icon>
-          <font-awesome-icon icon="chart-line" />
-        </template>
-        <router-link :to="{ path: 'search' }">Manage users</router-link>
-      </vs-sidebar-item>
-      <vs-sidebar-item id="market">
-        <template #icon>
-          <font-awesome-icon icon="chart-line" />
-        </template>
-        <router-link :to="{ path: 'create' }">Create user</router-link>
-      </vs-sidebar-item>
-      <vs-sidebar-item id="market">
-        <template #icon>
-          <font-awesome-icon icon="chart-line" />
-        </template>
-        <router-link :to="{ path: 'workingtime' }">Manage WorkingTime</router-link>
-      </vs-sidebar-item>
+      <router-link :to="{ path: 'dashboard' }">
+        <vs-sidebar-item id="dashboard">
+          <template #icon>
+            <font-awesome-icon icon="chart-line" />
+          </template>
+          Dashboard
+        </vs-sidebar-item>
+      </router-link>
+      <router-link :to="{ path: 'search' }">
+        <vs-sidebar-item id="search">
+          <template #icon>
+            <font-awesome-icon icon="chart-line" />
+          </template>
+          Manage users
+        </vs-sidebar-item>
+      </router-link>
+      <router-link :to="{ path: 'create' }">
+        <vs-sidebar-item id="market">
+          <template #icon>
+            <font-awesome-icon icon="chart-line" />
+          </template>
+          Create user
+        </vs-sidebar-item>
+      </router-link>
+      <router-link :to="{ path: 'workingtime' }">
+        <vs-sidebar-item id="market">
+          <template #icon>
+            <font-awesome-icon icon="chart-line" />
+          </template>
+          Manage WorkingTime
+        </vs-sidebar-item>
+      </router-link>
     </vs-sidebar>
   </div>
 </template>
@@ -44,3 +52,10 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+button:focus {
+  outline: 0 !important;
+  outline: unset;
+}
+</style>
