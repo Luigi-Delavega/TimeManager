671b94a077dfa89a71560e9c31bdadcf9dd59657
crg63 <floriancargouet@gmail.com>
2020-10-24 19:28:52 +0200

    get user by id

--- theme01/lib/gotham_web/router.ex
+++ theme01/lib/gotham_web/router.ex
@@ -12,6 +12,7 @@ defmodule GothamWeb.Router do
     pipe_through :api
 
     get "/users", UserController, :getAll
+    get "/user/id/:id", UserController, :get_user
     get "/user/:username", UserController, :show_by_username
     resources "/users", UserController, except: [:new, :edit]
     # options "/users", UserController, :create
